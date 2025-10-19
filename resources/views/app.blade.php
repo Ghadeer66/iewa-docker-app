f1a86ec90119fe2fbe8356498d099db6cea877d4
ghadeerghanem77 <ghadeerghanem77@gmail.com>
2025-10-19 14:03:12 +0330

    fixing vit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     @if (app()->environment('production'))
-        <!-- Production assets -->
+        <!-- Production assets - Remove APP_URL from asset path -->
         <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
         <script type="module" src="{{ asset('build/assets/app.js') }}"></script>
     @else
